99b2e22ab7dcff5b9127f9324677b855fd0fb560
Rogério Nunes Costa <60291930+rogerioncosta@users.noreply.github.com>
2023-09-27 00:24:35 -0300

    Add files via upload

---
+++ src/test/java/paginas/ListaDeProdutosPage.java
@@ -0,0 +1,23 @@
+package paginas;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class ListaDeProdutosPage {
+    private WebDriver navegador;
+
+    public ListaDeProdutosPage(WebDriver navegador) {
+        this.navegador = navegador;
+    }
+
+    public FormularioDeAdicaoDeProdutoPage acessarFormularioAdicaoNovoProduto() {
+        // Ir para a tela de registro de produto
+        navegador.findElement(By.linkText("ADICIONAR PRODUTO")).click();
+
+        return new FormularioDeAdicaoDeProdutoPage(navegador);
+    }
+
+    public String capturarMensagemApresentada() {
+        return navegador.findElement(By.cssSelector(".toast.rounded")).getText();
+    }
+}
